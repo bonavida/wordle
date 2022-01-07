27044e3e90f30be06ba2b7b8b44ce7df5c400763
Diego Bonavida <gecko.bona@gmail.com>
2022-01-07 23:05:22 +0100

    Add layout and header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <h1>Wordle</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 10px 20px;
+  border-bottom: 1px solid #d3d3d4;
+}
+
+h1 {
+  color: #003543;
+  font-size: 28px;
+  font-weight: 700;
+}
+</style>
---
+++ src/layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="layout">
+    <Header />
+    <div class="content">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  width: 100%;
+  max-width: 700px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
+
+.content {
+  max-width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+  margin: 30px auto;
+}
+</style>

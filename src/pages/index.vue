88095847c913e2ea6d7616d8236384219cfbb98f
Diego Bonavida <gecko.bona@gmail.com>
2022-01-19 01:57:47 +0100

    Make confetti rain when the users wins the game

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,49 @@
 <template>
-  <div class="content">
+  <div ref="main" class="content">
     <div class="wrapper">
       <Board />
     </div>
     <Keyboard />
+    <canvas ref="confetti" class="canvas" width="500" height="500"></canvas>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { GAME_STATUS } from '@constants/game';
+
+const canvasConfetti = require('canvas-confetti');
 
 export default Vue.extend({
   name: 'IndexPage',
+  data() {
+    return {
+      confetti: (options: any) => options,
+      unwatch: () => {},
+    };
+  },
+  mounted() {
+    this.confetti = canvasConfetti.create(this.$refs.confetti);
+
+    // Fire confetti when the users win
+    this.unwatch = this.$store.watch(
+      (state) => state.status,
+      (newValue) => {
+        if (newValue === GAME_STATUS.WIN) {
+          // Make it rain!
+          this.confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 1 },
+            resize: true,
+          });
+        }
+      }
+    );
+  },
+  beforeDestroy() {
+    this.unwatch();
+  },
 });
 </script>
 
@@ -34,6 +66,12 @@ export default Vue.extend({
   justify-content: center;
   align-items: center;
   flex: 1 1 auto;
+}
+
+.canvas {
+  position: absolute;
+  bottom: 0;
+  pointer-events: none;
 }
 
 @media (max-width: 480px) {
